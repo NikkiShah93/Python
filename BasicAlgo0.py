4e9a3640ee020b3f91fd8ed54d9745441ec2a2cf
Negar Shahrokh <neg.shahrokh@gmail.com>
2023-02-16 10:19:36 -0800

    Create BasicAlgo0.py
    Basic algo tests

---
+++ BasicAlgo0.py
@@ -0,0 +1,17 @@
+## The main function of this code is to figure out if a value 
+## is present in a file or not;
+## Simplest trick in the book
+## divide and conquer!
+
+def find_target(file, target):
+    if len(file) == 0:
+        return False
+    else:
+        n = len(file)//2
+        file.sort()
+        if file[n] == target:
+            return True
+        elif file[n] > target:
+            return find_target(file[:n], target)
+        else:
+            return find_target(file[n+1:], target)
