65a919f73bed3e527a0e33d9cf7875404425adab
Negar Shahrokh <neg.shahrokh@gmail.com>
2023-12-18 21:03:26 -0500

    working on Apple stock

---
+++ stockPrediction/src/predictionModel.py
@@ -0,0 +1,16 @@
+## we'll use Yahoo Finance to get the data
+import yfinance as yf 
+## then we pass in a symbol for the stock we want to use
+## this case Apple
+apple = yf.Ticker("AAPL")
+## this is an object
+## we need to then query the historical data
+apple = apple.history(perid = "max")
+## this will give us a pandas df
+## so we can use it like one
+## first add the tomorrow's value to each row
+apple["tomorrow"] = apple["Open"].shift(-1)
+## we also need to add a flag to see if it's up or down
+apple["target"] = (apple["Open"] < apple['tomorrow']).astype(int)
+
+
