388d80fa887bea84e3a619c68fe01b459c0bbb74
Negar Shahrokh <neg.shahrokh@gmail.com>
2023-12-21 21:41:20 -0500

    downloading the SP500 data

--- PredictionModels/scripts/algorithmicTrading.py
+++ PredictionModels/scripts/algorithmicTrading.py
@@ -23,3 +23,14 @@ sp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
 ## replacing dots with dashes and
 ## getting rid of the spaces, and making sure they're all uppercased
 sp500['Symbol'] = sp500['Symbol'].str.replace('.','-').str.upper().str.strip()
+## and then getting all the unique values, as a list
+symbols_list = sp500['Symbol'].unique().tolist()
+
+## lets define our start and end dates
+## going back 8 years
+end_date = '2023-12-20'
+start_date = pd.to_datetime(end_date) - pd.DateOffset(365*8)
+## lets download the SP500 stock prices
+data = yf.download(tickers = symbols_list,
+                  start = start_date,
+                  end = end_date)
