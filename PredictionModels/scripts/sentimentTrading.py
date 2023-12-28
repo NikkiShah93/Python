dcd3814d9dcce52a3a9e1247f1a1808ab752a7bf
Negar Shahrokh <neg.shahrokh@gmail.com>
2023-12-28 11:22:06 -0500

    loaded and cleaned the data

--- PredictionModels/scripts/sentimentTrading.py
+++ PredictionModels/scripts/sentimentTrading.py
@@ -10,3 +10,16 @@ import datetime
 import yfinance as yf
 import os
 plt.style.use('ggplot')
+## path to the twitter data
+data_path = '../data/sentiment_data.csv'
+sentiment_data = pd.read_csv(data_path)
+sentiment_data['date'] = pd.to_datetime(sentiment_data['date'])
+sentiment_data = sentiment_data.set_index(['date', 'symbol'])
+## lets calculate the engagement ratio
+## by dividing the comments by likes
+## to have a way to filter
+## the bot activities
+sentiment_data['engagement_ratio'] = sentiment_data['twitterComments'].div(sentiment_data['twitterLikes'])
+## then we want to filter out
+## low likes/comments stocks
+sentiment_data = sentiment_data[(sentiment_data['twitterLikes'] > 20) & (sentiment_data['twitterComments']>5)]
