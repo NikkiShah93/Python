39651969b31c44a5894c01bd0e87352c2c559409
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-01-01 11:42:05 -0500

    defining and fitting the GARCH model

--- PredictionModels/scripts/intradayTrading.py
+++ PredictionModels/scripts/intradayTrading.py
@@ -30,5 +30,34 @@ daily_df = daily_df.drop('Unnamed: 7', axis =1)
 intraday_5min_df = pd.read_csv(min_data_path)
 intraday_5min_df['datetime'] = pd.to_datetime(intraday_5min_df['datetime'])
 intraday_5min_df.drop('Unnamed: 6', axis= 1, inplace = True)
-intraday_5min_df['log_ret'] = np.log(intraday_5min_df['close']).diff()
 intraday_5min_df = intraday_5min_df.set_index('datetime')
+intraday_5min_df['date'] = intraday_5min_df.index.date
+## now we want to define a func 
+## to fit the GARCH model 
+## and predict 1-day ahead volatility 
+## in a rolling window
+## we will first calculate the 6-month
+## rolling variance and then
+## we are creating a function
+## in a 6-month rolling window to fit 
+## a garch model and predict
+## the next day variance
+## we need to test to find 
+## what orders would work best 
+## first, calculate the 6-month rolling variance
+daily_df['variance'] = daily_df['Adj Close'].rolling(180).var()
+## for simplicity
+## we will only use 2020 forward
+daily_df = daily_df['2020-01-01':]
+## and lets define the function
+def predict_volatility(x):
+    ## p is auto regressive order
+    ## q is moving avg order
+    garch_model = arch_model(y=x, p=1, q=3).fit(update_freq = 5, disp = 'off')
+    ## then we want to forcast one day ahead
+    ## and it will generate mean and var
+    ## and we want to only get var
+    variance_forcast = garch_model.forecast(horizon=1).variance.iloc[-1,0]
+    return variance_forcast
+## now we want to get the predictions of the model
+daily_df['predictions'] = daily_df['Adj Close'].rolling(180).apply(lambda x : predict_volatility(x))
