fea68f5c4249c3a0eb143d4691678e36ae7c2e9b
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-01-10 18:27:20 -0500

    downloading the data and calculating returns

--- PredictionModels/scripts/stockAnalysis.py
+++ PredictionModels/scripts/stockAnalysis.py
@@ -45,7 +45,34 @@ def save_csv_from_yahoo(folder, ticker):
 ## getting the tickers for Wilshire
 ## in the data folder
 data_path = "../data"
+stock_data_path = f"{data_path}/stock_data"
 tickers = get_column_from_csv(f"{data_path}/Wilshire-5000-Stocks.csv", "Ticker")
 
 for ticker in tickers:
-    save_csv_from_yahoo(folder = f"{data_path}/stock_data", ticker=ticker)
+    save_csv_from_yahoo(folder = stock_data_path, ticker=ticker)
+## end and start dates
+start_date = dt.date.today() - pd.Dateoffset(months = 36)
+end_date = dt.date.today()
+## getting a df from csv
+def get_stock_df_from_csv(folder, ticker):
+    try:
+        ## and getting the first colunn as index
+        file_name = f"{folder}/{ticker.replace('.','_')}.csv"
+        df = pd.read_csv(file_name, index_col = 0)
+    except Exception as e:
+        print(f"{file_name} had {e} issue!")
+    else:
+        return df
+## only getting the stocks that have been downloaded
+files = [f for f in os.listdir(stock_data_path) if f.endswith('.csv') and os.isfile(join(stock_data_path, f))]
+tickers = [os.path.splitext(x)[0] for x in files if os.path.splitext(x)[0]!= '.ds_Store']
+## we're only interested in returns
+def add_daily_return(df):
+    ## today's close / yesterday's
+    df['daily_return'] = (df['Close']/df['Close'].shift(1)) - 1
+    ## or np.log(df['Close']).diff()
+    return df
+def add_cum_return(df):
+    df['cum_return'] = (1 + df['daily_return']).cumprod()
+    ## or np.exp(np.log1p(df['daily_return']).cumsum()).sub(1)
+    return df
